ee11c05d2ab21939fffe66872cf0ded637ebfb8f
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-05-25 13:07:08 +0600

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -205,11 +205,8 @@ DOWNLOAD_FIRMWARE() {
     fi
 
     # --- Show Firmware Info ---
-    local file_size=$(du -m "${DOWN_DIR}/${MODEL}_*_fac.zip" | cut -f1)
-    echo -e "Firmware Size: ${file_size} MB"
-
-    # --- Cleanup ---
-    rm -f "$enc_file"
+    local file_size=$(du -m ${DOWN_DIR}/${MODEL}_*_fac.zip 2>/dev/null | cut -f1)
+    echo "Firmware Size: ${file_size:-0} MB"
 }
 
 
