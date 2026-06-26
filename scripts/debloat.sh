6818e5700c592103b0af5823059391ffae5538ee
JorgeSL2011 <jorgelunaguerrero2011@gmail.com>
2026-06-26 11:11:21 -0500

    Update debloat.sh

--- scripts/debloat.sh
+++ scripts/debloat.sh
@@ -67,7 +67,7 @@ GOOGLE_APPS=(
 "YourPhone_Stub" "AndroidAutoStub" "FamilyLinkParentalControls"
 "AndroidSystemIntelligence" "GoogleRestore"
 "SamsungMessages" "SearchSelector" "PlayAutoInstallConfig" "FamilyLinkParentalControls"
-"Gmail2" "Velvet" "YouTtube"
+"Gmail2" "Velvet" "YouTube"
 )
 
 
