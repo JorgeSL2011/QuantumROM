ecfab68c9e305a6125f2ed7c74194e2ba527ef00
JorgeSL2011 <jorgelunaguerrero2011@gmail.com>
2026-06-26 17:02:12 -0500

    Update sixteen.sh

--- sixteen.sh
+++ sixteen.sh
@@ -61,6 +61,7 @@ mv -f "$WORK_DIR"/*.jar "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/"
 
 PATCH_BT_LIB "$FIRM_DIR/$TARGET_DEVICE" "$WORK_DIR"
 FIX_CAMERA "$FIRM_DIR/$TARGET_DEVICE"
+FIX_BLUETOOTH "$FIRM_DIR/$TARGET_DEVICE"
 
 B_ID="$(grep -m1 '^ro.system.build.id=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
 B_V="$(grep -m1 '^ro.system.build.version.incremental=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
