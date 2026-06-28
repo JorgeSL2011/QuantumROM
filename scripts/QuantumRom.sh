bc227fe386bbbb6f751d4c6ba0148aa378ab9a4c
JorgeSL2011 <jorgelunaguerrero2011@gmail.com>
2026-06-28 10:47:29 -0500

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -2407,7 +2407,6 @@ BUILD_IMG() {
                 --file-contexts="$FILE_CONTEXTS" \
                 -z lz4hc,12 \
                 -E fragments \
-                --all-fragments \
                 -b 4096 \
                 -T 1199145600 \
                 "$OUT_IMG" "$SOURCE_DIR"
